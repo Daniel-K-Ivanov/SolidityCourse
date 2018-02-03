bd0f9eb1acc2992206e7841d33d21becf95d1e6f
Daniel Ivanov <daniel.k.ivanov95@gmail.com>
2018-02-03 18:56:05 +0200

    state variable example

--- MathContact.sol
+++ MathContact.sol
@@ -2,28 +2,43 @@ pragma solidity 0.4.19;
 
 contract MathContract {
     
-    function add(int256 a, int256 b) public returns (int256) {
-        return a + b;
+    int256 a = 0;
+    
+    
+    function reInitVariable() public {
+        a = 0;
     }
     
-    function substract(int256 a, int256 b) public returns (int256) {
-        return a - b;
+    function getVariableValue() public returns (int256) {
+        return a;
     }
     
-    function multiply(int256 a, int256 b) public returns (int256) {
-        return a * b;
+    //TODO add overflow
+    function add(int256 b) public {
+        a += b;
     }
     
-    function divide(int256 a, int256 b) public returns (int256) {
-        // add b = 0 check?
-        return a / b;
+    //TODO add underflow
+    function substract(int256 b) public {
+        a -= b;
     }
     
-    function raise(uint256 a, uint256 b) public returns (uint256) {
-        return a ** b;
+    //TODO add overflow
+    function multiply(int256 b) public {
+        a *= b;
     }
     
-    function divideAndRetrieveDiff(int256 a, int256 b) public returns (int256) {
-        return a % b;
+    //TODO add divide by 0 check
+    function divide(int256 b) public {
+        a /= b;
+    }
+    
+    //TOOD check for overflow
+    function raise(uint256 b) public {
+        //a = a ** b;
+    }
+    
+    function divideAndRetrieveDiff(int256 b) public {
+        a %= b;
     }
 }
